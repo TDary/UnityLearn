5963857e7406a7afcb87c83db5397b4fc1bbcb92
chenDary <10444087+chendary@user.noreply.gitee.com>
2022-08-29 14:57:47 +0800

    update Element

---
+++ MyProject/Assets/scripts/SliderTest.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SliderTest : MonoBehaviour
+{
+    public Slider slider;
+    public Text c_text;
+    private float sliderValue;
+    private float speed = 0.5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        //≥ı ºªØ
+        sliderValue = slider.value;
+        sliderValue = 0;
+    }
+
+    private void ChangeSlider()
+    {
+        if(sliderValue < 100)
+        {
+            sliderValue += 0.02f;
+            c_text.text = sliderValue + "%";
+            slider.value = sliderValue / 100;
+        }
+        else
+        {
+            c_text.text = "OK";
+            sliderValue = 0;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ChangeSlider();
+    }
+}
